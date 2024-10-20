75c7c3d2e75ecf06a289d0b932409043f7f05a42
Arren De Manuel <arrendeman@gmail.com>
2024-10-20 13:55:24 -0700

    Add CNAME and contact info

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,8 @@ export function HomePage() {
   return (
     <>
       <h1>Welcome to Rlyn's shop!</h1>
-      <p>This shop is still under construction</p>
+      <p>This shop is still under construction.</p>
+      <p>For inquiries, please contact Pelagias.pride24@gmail.com</p>
     </>
   );
 }
