86c70d579604b33ac2c7ca83bfe43be39826623e
Arren De Manuel <arrendeman@gmail.com>
2024-10-21 19:01:51 -0700

    Add a background image

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,19 @@
+const homeStyle: React.CSSProperties = {
+  backgroundColor: '#E6E8E6',
+  borderRadius: '20px',
+  paddingTop: '20px',
+  paddingBottom: '20px',
+  paddingLeft: '30px',
+  paddingRight: '30px',
+};
+
 export function HomePage() {
   return (
-    <>
+    <div style={ homeStyle }>
       <h1>Welcome to Rlyn's shop!</h1>
       <p>This shop is still under construction.</p>
       <p>For inquiries, please contact Pelagias.pride24@gmail.com</p>
-    </>
+    </div>
   );
 }
 
