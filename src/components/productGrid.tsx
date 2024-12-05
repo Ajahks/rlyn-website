fae27e2c47904f44e7f8958e6901884b394c4902
Arren De Manuel <ademanue@ucsd.edu>
2024-12-05 11:42:23 -0800

    Refactor products to its own file

--- src/components/productGrid.tsx
+++ src/components/productGrid.tsx
@@ -2,49 +2,17 @@ import "./css/product.css"
 import { Grid2 } from "@mui/material";
 import { Product } from "../types/product";
 import ProductCard from "./productCard";
+import {products} from "../data/products.tsx";
 
 interface ProductGridProps {
   onClick: (product: Product) => void;
 }
 
-const sampleProducts: Product[] = [
-  {
-    name: "Bingo Bags", 
-    price: "Contact for more info", 
-    productCardImg: "/products/bingo-bag/bingo-bag-productCard.png",
-    productImgs: [
-      "/products/bingo-bag/bingo-bag-productCard.png",
-      "/products/bingo-bag/bingo-bag1-productImg.png",
-      "/products/bingo-bag/bingo-bag2-productImg.png",
-      "/products/bingo-bag/bingo-bag3-productImg.png"
-    ],
-  },
-  {
-    name: "Purses", 
-    price: "Contact for more info", 
-    productCardImg: "/products/purse/purse-productCard.JPG",
-    productImgs: [
-      "/products/purse/purse-productCard.JPG",
-      "/products/purse/purse1-productImg.JPG",
-      "/products/purse/purse2-productImg.png",
-      "/products/purse/purse3-productImg.png"
-    ],
-  },
-  {
-    name: "Tote Bags", 
-    price: "Contact for more info", 
-    productCardImg: "/products/tote/tote-productCard.JPG",
-    productImgs: [
-      "/products/tote/tote-productCard.JPG",
-    ],
-  },
-];
-
 const ProductGrid: React.FC<ProductGridProps> = ({onClick}) => {
   return (
     <div className="product-grid">
       <Grid2 container spacing={2}>
-        {sampleProducts.map((product) => (
+        {products.map((product) => (
           <Grid2 size={6}>
             <ProductCard product={product} onClick={onClick}/>
           </Grid2>
---
+++ src/data/products.tsx
@@ -0,0 +1,34 @@
+import {Product} from "../types/product.tsx";
+
+export const products: Product[] = [
+    {
+        name: "Bingo Bags",
+        price: "Contact for more info",
+        productCardImg: "/products/bingo-bag/bingo-bag-productCard.png",
+        productImgs: [
+            "/products/bingo-bag/bingo-bag-productCard.png",
+            "/products/bingo-bag/bingo-bag1-productImg.png",
+            "/products/bingo-bag/bingo-bag2-productImg.png",
+            "/products/bingo-bag/bingo-bag3-productImg.png"
+        ],
+    },
+    {
+        name: "Purses",
+        price: "Contact for more info",
+        productCardImg: "/products/purse/purse-productCard.JPG",
+        productImgs: [
+            "/products/purse/purse-productCard.JPG",
+            "/products/purse/purse1-productImg.JPG",
+            "/products/purse/purse2-productImg.png",
+            "/products/purse/purse3-productImg.png"
+        ],
+    },
+    {
+        name: "Tote Bags",
+        price: "Contact for more info",
+        productCardImg: "/products/tote/tote-productCard.JPG",
+        productImgs: [
+            "/products/tote/tote-productCard.JPG",
+        ],
+    },
+];
