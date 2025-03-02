ace142dd28199d7c0f80849eb9a9e0b0bdb131e9
Arren De Manuel <arrendeman@gmail.com>
2025-03-02 14:45:28 -0800

    Move separate large bingo bags product card

--- src/data/products.tsx
+++ src/data/products.tsx
@@ -2,19 +2,6 @@ import {Product} from "../types/product.tsx";
 
 export const products: Product[] = [
     {
-        name: "Bingo Bags",
-        shortDescription: "Custom, hand-made bags perfect for bingo.",
-        longDescription: `Custom hand made bingo bags.  Can be made with various fabrics of the customer's choosing. Can add strap for an extra $10`,
-        price: "$25",
-        productCardImg: "/products/bingo-bag/bingo-bag-productCard.png",
-        productImgs: [
-            "/products/bingo-bag/bingo-bag-productCard.png",
-            "/products/bingo-bag/bingo-bag1-productImg.png",
-            "/products/bingo-bag/bingo-bag2-productImg.png",
-            "/products/bingo-bag/bingo-bag3-productImg.png"
-        ],
-    },
-    {
         name: "Purses",
         price: "$40",
         shortDescription: "Custom, hand-made purses with strap.",
@@ -33,11 +20,38 @@ export const products: Product[] = [
         productCardImg: "/products/tote/tote-productCard.JPG",
         productImgs: [
             "/products/tote/tote-productCard.JPG",
-            "/products/tote/tote2-productImg.JPG",
-            "/products/tote/tote3-productImg.JPG",
         ],
         shortDescription: "Custom hand made tote bags",
         longDescription: `Custom hand made tote bag.  Can be made with various fabrics of the customer's choosing. Can add strap for an extra $10`,
+        dimensions: new Map([
+            ['Width', "15 inches"],
+            ['Height', "11 inches"],
+            ['Depth', "5 inches"],
+        ])
+    },
+    {
+        name: "Bingo Bags",
+        shortDescription: "Custom, hand-made bags perfect for bingo.",
+        longDescription: `Custom hand made bingo bags.  Can be made with various fabrics of the customer's choosing. Can add strap for an extra $10`,
+        price: "$25",
+        productCardImg: "/products/bingo-bag/bingo-bag-productCard.png",
+        productImgs: [
+            "/products/bingo-bag/bingo-bag-productCard.png",
+            "/products/bingo-bag/bingo-bag1-productImg.png",
+            "/products/bingo-bag/bingo-bag2-productImg.png",
+            "/products/bingo-bag/bingo-bag3-productImg.png"
+        ],
+    },
+    {
+        name: "Large Bingo Bags",
+        shortDescription: "Custom, large, hand-made bags perfect for bingo.",
+        longDescription: `Custom hand made bingo bags.  Same dimensions as the tote bag.  Can be made with various fabrics of the customer's choosing. Can add strap for an extra $10`,
+        price: "$40",
+        productCardImg: "/products/large-bingo-bag/tote3-productImg.JPG",
+        productImgs: [
+            "/products/large-bingo-bag/tote2-productImg.JPG",
+            "/products/large-bingo-bag/tote3-productImg.JPG",
+        ],
         dimensions: new Map([
             ['Width', "15 inches"],
             ['Height', "11 inches"],
